1eefd06db07e30233d95ac5875814bce389fb64d
Adrian Vazquez <adri.forzaferrari@gmail.com>
2016-03-02 00:40:36 +0100

    First test with sockets, only to see how they work. It is not even my own code

---
+++ server.py
@@ -0,0 +1,19 @@
+import socket  
+
+s = socket.socket()   
+s.bind(("localhost", 9999))  
+s.listen(1)  
+
+sc, addr = s.accept()  
+
+while True:  
+  recibido = sc.recv(1024)  
+  if recibido == "quit":  
+    break        
+  print "Message received:", recibido  
+  sc.send(recibido)  
+
+print "bye..."  
+
+sc.close()  
+s.close()  
