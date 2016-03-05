f8c890b22a2eb8c34deccd101663b05fdcac8c21
Luis <lhernandez@saluspot.com>
2016-03-05 13:06:25 +0100

    pep 8 again

--- client.py
+++ client.py
@@ -4,10 +4,10 @@ s = socket.socket()
 s.connect(("localhost", 9999))
 
 while True:
-	mensaje = raw_input("> ")
-	s.send(mensaje)
-	if mensaje == "quit":
-		break  
+    mensaje = raw_input("> ")
+    s.send(mensaje)
+    if mensaje == "quit":
+        break
 
 print "bye..."
 
